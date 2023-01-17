d2e7537a210199d258e50dfe08ab084bf7e2032a
poros <poros@DESKTOP-VS58TJL>
2023-01-18 00:00:06 +0200

    Добавьте файлы проекта.

---
+++ Assignment4_NikitaBoborenko/Form1.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace Individual_Assignment_4
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+    
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.inputButton = new System.Windows.Forms.Button();
+            this.InputTextBox = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // inputButton
+            // 
+            this.inputButton.Location = new System.Drawing.Point(308, 166);
+            this.inputButton.Margin = new System.Windows.Forms.Padding(2);
+            this.inputButton.Name = "inputButton";
+            this.inputButton.Size = new System.Drawing.Size(56, 19);
+            this.inputButton.TabIndex = 0;
+            this.inputButton.Text = "Input";
+            this.inputButton.UseVisualStyleBackColor = true;
+            this.inputButton.Click += new System.EventHandler(this.inputButton_Click);
+            // 
+            // InputTextBox
+            // 
+            this.InputTextBox.Location = new System.Drawing.Point(228, 166);
+            this.InputTextBox.Margin = new System.Windows.Forms.Padding(2);
+            this.InputTextBox.Name = "InputTextBox";
+            this.InputTextBox.Size = new System.Drawing.Size(76, 20);
+            this.InputTextBox.TabIndex = 4;
+            this.InputTextBox.TextChanged += new System.EventHandler(this.InputTextBox_TextChanged_1);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(600, 366);
+            this.Controls.Add(this.InputTextBox);
+            this.Controls.Add(this.inputButton);
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button inputButton;
+        private System.Windows.Forms.TextBox InputTextBox;
+    }
+}
+
---
+++ Assignment4_NikitaBoborenko/Form1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace Individual_Assignment_4
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void InputTextBox_TextChanged_1(object sender, EventArgs e)
+        {
+            if (System.Text.RegularExpressions.Regex.IsMatch(InputTextBox.Text, "[^0-9]"))
+            {
+                MessageBox.Show("You can enter only numbers");
+                InputTextBox.Text = InputTextBox.Text.Remove(InputTextBox.Text.Length - 1);
+            }
+        }
+
+        private void inputButton_Click(object sender, EventArgs e)
+        {
+            try {
+                int result = 1;
+                for (int i = Convert.ToInt32(InputTextBox.Text); i > 0; i--)
+                    result *= i;
+                MessageBox.Show(Convert.ToString(result));
+
+            }
+            catch {
+                MessageBox.Show("Input is empty");
+            }
+            
+        }
+
+       
+    }
+}
